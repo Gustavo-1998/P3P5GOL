79b7c3add219a96c2ebc51c6e719b279cb07679b
Gustavo Ortega Lopez <ortegalopezgustavo1998@gmail.com>
2020-01-06 23:58:22 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/gol/p3p5gol/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.isc.gol.p3p5gol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
